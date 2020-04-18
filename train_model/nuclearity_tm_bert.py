4bf8fa9a159ee9622efbaf041b742aa0f4c67f74
liuxuan320 <jf940927320@163.com>
2020-04-18 09:31:08 +0800

    修改模型名
    修改模型文件的名字使得名字统一，并且再测试时不让数据进行打乱，保证评估的正确性。

--- train_model/nuclearity_tm_bert.py
+++ train_model/nuclearity_tm_bert.py
@@ -10,62 +10,58 @@ from utils.file_util import create_dir
 from utils.metric_util import write_result
 
 
-def relation_main(data_file_name, save_model_root, model, tokenizer, relation_dict):
+def nuclearity_main(data_file_name, save_model_root, model, tokenizer, nuclearity_dict):
+    """
+    主次实验主过程
+    :param data_file_name:
+    :param save_model_root:
+    :param model:
+    :param tokenizer:
+    :param nuclearity_dict:
+    :return:
+    """
     maxlen = 512
     epoch = 5
     train_data, test_data = get_data(data_file_name)
+    train_data = del_none_data(train_data, datatype="nuclearity")
     print("完成数据加载")
-    train_data = del_none_data(train_data, datatype="relation")  # 这句与结构不同
     train_d = DataGenerator(train_data, batch_size=2,
-                            relation_dict=relation_dict,
+                            relation_dict=nuclearity_dict,
                             tokenizer=tokenizer,
                             maxlen=maxlen,
-                            data_type="relation")
+                            data_type="nuclearity")
     print("完成迭代器转换")
     model.compile(
         loss='categorical_crossentropy',
         optimizer=Adam(1e-5),  # 用足够小的学习率
         metrics=['accuracy']
     )
-    create_dir(save_model_root)
     print("完成模型构建")
+    create_dir(save_model_root)
     for i in range(epoch):
         model.fit_generator(
             train_d.__iter__(),
-            steps_per_epoch=len(train_d)
+            steps_per_epoch=len(train_d),
         )
         save_file = save_model_root + "/save_model" + str(i) + "epoch.model"
         model.save_weights(save_file)
 
 
-def test_model(data_file_name, load_model_root, model, tokenizer, relation_dict, index=1):
+def test_model(data_file_name, load_model_root, model, tokenizer, nuclearity_dict, index=1):
+    print(index)
     maxlen = 512
     train_data, test_data = get_data(data_file_name)
-    test_data = del_none_data(test_data, datatype="relation")
-    test_d = DataGenerator(test_data, batch_size=2, relation_dict=relation_dict, tokenizer=tokenizer, maxlen=maxlen,
-                           data_type="relation")
+    test_data = del_none_data(test_data, datatype="nuclearity")
+    test_d = DataGenerator(test_data, batch_size=2, relation_dict=nuclearity_dict, tokenizer=tokenizer, maxlen=maxlen,
+                           data_type="nuclearity", shuffle=False)
     model_name = load_model_root + "/save_model" + str(index) + "epoch.model"
+    print(model_name)
     model.load_weights(model_name)
     temp_result = model.predict_generator(test_d.__iter__(), steps=len(test_d)).argmax(axis=-1)
-    golden_label = get_test_golden_label(test_data, relation_dict, datatype="relation")
+    golden_label = get_test_golden_label(test_data, nuclearity_dict)
     output_golden_file = load_model_root + "/result" + str(index) + ".txt"
-    relation_list = ['Joint',
-                     'Sequence',
-                     'Progression',
-                     "Contrast",
-                     "Supplement",
-                     "Cause-Result",
-                     "Result-Cause",
-                     "Background",
-                     "Behavior-Purpose",
-                     "Purpose-Behavior",
-                     "Elaboration",
-                     "Summary",
-                     "Evaluation",
-                     "Statement-Illustration",
-                     "Illustration-Statement"
-                     ]
-    write_result(output_golden_file, golden_label, temp_result, label_name=relation_list)
+    relation_list = ['NS', 'SN', 'NN']
+    write_result(output_golden_file, golden_label, temp_result, relation_list)
     del model
 
 
@@ -76,10 +72,11 @@ if __name__ == '__main__':
     create_dir(root)
     data_file_name = ""
     model_root = ""
-    relation_dict = ParsingIndex.relation_dict
     config_path, checkpoint_path, tokenizer = get_config_path_and_checkpoint_path_and_tokenizer()
+    nuclearity_dict = ParsingIndex.nuclearity_dict
+    TM_bert_model = build_TM_bert_model(config_path, checkpoint_path, num_classes=len(nuclearity_dict.items()))
     keras.backend.clear_session()
-    TM_bert_model = model = build_TM_bert_model(config_path, checkpoint_path, num_classes=len(relation_dict.items()))
-    relation_main(data_file_name, model_root, TM_bert_model, tokenizer, relation_dict)
+    nuclearity_main(data_file_name, model_root, TM_bert_model, tokenizer, nuclearity_dict)
     for i in range(0, 5):
-        test_model(data_file_name, model_root, TM_bert_model, tokenizer, relation_dict, index=i)
+        print(i)
+        test_model(data_file_name, model_root, TM_bert_model, tokenizer, nuclearity_dict, index=i)
---
+++ train_model/relation_tm_bert.py
@@ -0,0 +1,85 @@
+from keras.optimizers import Adam
+
+from model.tm_bert import build_TM_bert_model
+from train_model.load_data import get_data, del_none_data, get_test_golden_label
+
+from model.tm_bert import DataGenerator
+from parsing.parsing_index import ParsingIndex
+from train_model.model_prepare import get_config_path_and_checkpoint_path_and_tokenizer
+from utils.file_util import create_dir
+from utils.metric_util import write_result
+
+
+def relation_main(data_file_name, save_model_root, model, tokenizer, relation_dict):
+    maxlen = 512
+    epoch = 5
+    train_data, test_data = get_data(data_file_name)
+    print("完成数据加载")
+    train_data = del_none_data(train_data, datatype="relation")  # 这句与结构不同
+    train_d = DataGenerator(train_data, batch_size=2,
+                            relation_dict=relation_dict,
+                            tokenizer=tokenizer,
+                            maxlen=maxlen,
+                            data_type="relation")
+    print("完成迭代器转换")
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=Adam(1e-5),  # 用足够小的学习率
+        metrics=['accuracy']
+    )
+    create_dir(save_model_root)
+    print("完成模型构建")
+    for i in range(epoch):
+        model.fit_generator(
+            train_d.__iter__(),
+            steps_per_epoch=len(train_d)
+        )
+        save_file = save_model_root + "/save_model" + str(i) + "epoch.model"
+        model.save_weights(save_file)
+
+
+def test_model(data_file_name, load_model_root, model, tokenizer, relation_dict, index=1):
+    maxlen = 512
+    train_data, test_data = get_data(data_file_name)
+    test_data = del_none_data(test_data, datatype="relation")
+    test_d = DataGenerator(test_data, batch_size=2, relation_dict=relation_dict, tokenizer=tokenizer, maxlen=maxlen,
+                           data_type="relation", shuffle=False)
+    model_name = load_model_root + "/save_model" + str(index) + "epoch.model"
+    model.load_weights(model_name)
+    temp_result = model.predict_generator(test_d.__iter__(), steps=len(test_d)).argmax(axis=-1)
+    golden_label = get_test_golden_label(test_data, relation_dict, datatype="relation")
+    output_golden_file = load_model_root + "/result" + str(index) + ".txt"
+    relation_list = ['Joint',
+                     'Sequence',
+                     'Progression',
+                     "Contrast",
+                     "Supplement",
+                     "Cause-Result",
+                     "Result-Cause",
+                     "Background",
+                     "Behavior-Purpose",
+                     "Purpose-Behavior",
+                     "Elaboration",
+                     "Summary",
+                     "Evaluation",
+                     "Statement-Illustration",
+                     "Illustration-Statement"
+                     ]
+    write_result(output_golden_file, golden_label, temp_result, label_name=relation_list)
+    del model
+
+
+if __name__ == '__main__':
+    import keras
+
+    root = "./output"
+    create_dir(root)
+    data_file_name = ""
+    model_root = ""
+    relation_dict = ParsingIndex.relation_dict
+    config_path, checkpoint_path, tokenizer = get_config_path_and_checkpoint_path_and_tokenizer()
+    keras.backend.clear_session()
+    TM_bert_model = build_TM_bert_model(config_path, checkpoint_path, num_classes=len(relation_dict.items()))
+    relation_main(data_file_name, model_root, TM_bert_model, tokenizer, relation_dict)
+    for i in range(0, 5):
+        test_model(data_file_name, model_root, TM_bert_model, tokenizer, relation_dict, index=i)
